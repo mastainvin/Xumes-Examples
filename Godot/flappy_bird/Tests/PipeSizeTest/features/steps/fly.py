29fdf64b8ff05cf96794f8d518ba6f56ce5170ab
Vincent Mastain <61213519+mastainvin@users.noreply.github.com>
2024-06-22 17:00:23 +0200

    feat: add new exemples for new xumes features
    - script
    - godot inputs

--- Godot/flappy_bird/Tests/PipeSizeTest/features/steps/fly.py
+++ Godot/flappy_bird/Tests/PipeSizeTest/features/steps/fly.py
@@ -1,15 +1,17 @@
 import numpy as np
 import stable_baselines3
 from gymnasium import spaces
+
 from xumes import Agent
+from xumes import GodotAction
 
 
 class Fly(Agent):
 
-    def __init__(self):
+    def __init__(self, model_path: str = None):
         super().__init__(
             observation_space=spaces.Dict({
-                "velocity": spaces.Box(-float('inf'), float('inf'), shape=(2,), dtype=float),
+                "velocity": spaces.Box(-float('inf'), 300, shape=(2,), dtype=float),
                 "X": spaces.Box(-float('inf'), float('inf'), shape=(1,), dtype=float),
                 "Y": spaces.Box(-float('inf'), float('inf'), shape=(1,), dtype=float),
                 "Y1": spaces.Box(-float('inf'), float('inf'), shape=(1,), dtype=float),
@@ -19,32 +21,35 @@ class Fly(Agent):
             total_timesteps=int(3_000_000),
             algorithm_type="MultiInputPolicy",
             algorithm=stable_baselines3.PPO,
-            save_path="./Tests/PipeSizeTest/models/fly",
+            save_path=model_path if model_path is not None else "./Tests/PipeSizeTest/models/fly",
             eval_freq=10000,
-            )
+        )
 
         self.points = 0
 
     def observation(self):
-        velocity = self.Game.Bird.velocity
-        return {"velocity": np.array(velocity, dtype=float), "X": np.array([self.X], dtype=float), "Y": np.array([self.Y], dtype=float), "Y1": np.array([self.Y1], dtype=float)}
+        velocity = self.context.Game.Bird.velocity
+        return {"velocity": np.array(velocity, dtype=float),
+                "X": np.array([self.context.X], dtype=float),
+                "Y": np.array([self.context.Y], dtype=float),
+                "Y1": np.array([self.context.Y1], dtype=float)}
 
     def reward(self):
-        if self.score > self.points:
-            self.points = self.score
+        if self.context.score > self.points:
+            self.points = self.context.score
             return 1
 
-        if self.dead or self.Game.Bird.position[1] < 0:
+        if self.context.dead or self.context.Game.Bird.position[1] < 0:
             return -1
         return 0
 
     def terminated(self):
-        terminated = self.dead != 0 or self.score >= 2 or self.Game.Bird.position[1] < 0
+        terminated = self.context.dead != 0 or self.context.score >= 2 or self.context.Game.Bird.position[1] < 0
         if terminated:
             self.points = 0
         return terminated
 
     def actions(self, raw_actions):
         if raw_actions == 1:
-            return [{"type": "ACTION_EVENT", "action_name": "jump"}]
+            return [GodotAction("jump")]
         return []
---
+++ Godot/flappy_bird/Tests/PipeSizeTest/features_script/steps/fly.py
@@ -0,0 +1,51 @@
+import random
+from typing import List
+
+import numpy as np
+
+from xumes import Script
+from xumes import Input
+
+from xumes import GodotAction
+
+from Tests.PipeSizeTest.features.steps.fly import Fly
+
+
+class FlyScript(Script):
+
+    def __init__(self):
+        super().__init__()
+        self.model = Fly(model_path="./Tests/PipeSizeTest/models/fly")
+        self.model2 = Fly(model_path="./Tests/PipeSizeTest/models/fly")
+
+    def step(self) -> List[Input]:
+
+        # One in ten times, the bird jumps randomly
+        if random.random() < 0.1:
+            if random.random() < 0.5:
+                return [GodotAction("jump")]
+            return []
+        observation = self.observation()
+
+        # use 2 different models to predict the action
+        if self.context.score % 2 == 0:
+            action = self.model2.predict(observation)
+        else:
+            action = self.model.predict(observation)
+
+        # convert the action to the game action
+        if action == 1:
+            return [GodotAction("jump")]
+
+        return []
+
+    def observation(self):
+        # convert the game state to an observation
+        velocity = self.context.Game.Bird.velocity
+        return {"velocity": np.array(velocity, dtype=float),
+                "X": np.array([self.context.X], dtype=float),
+                "Y": np.array([self.context.Y], dtype=float),
+                "Y1": np.array([self.context.Y1], dtype=float)}
+
+    def terminated(self):
+        return self.context.dead != 0 or self.context.score >= 2 or self.context.Game.Bird.position[1] < 0
